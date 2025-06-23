aa30c480d1c65094e32b8759b3090ad7450351d8
Kushal <kushalmohith09.com>
2025-06-23 14:41:36 +0530

    Content changed

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return jsonify({"message": "Hello from Flask!"})
+    return jsonify({"message": "Hello World!! "})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
