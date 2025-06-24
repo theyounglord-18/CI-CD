418babf92d7d60f5041557adfe6094c83884ab86
Kushal <kushalmohith09.com>
2025-06-24 09:06:02 +0530

    Test case  Changed

--- test.py
+++ test.py
@@ -4,4 +4,4 @@ def test_hello():
     client = app.app.test_client()
     response = client.get("/")
     assert response.status_code == 200
-    assert response.get_json() == {"message": "Hello World!"}
+    assert response.get_json() == {"message": "Hello World"}
